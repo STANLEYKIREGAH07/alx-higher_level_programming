dece4cf0b8cc5e78bd9e6255bc10b289c621e53c
STANLEYKIREGAH07 <stankirega@gmail.com>
2023-06-06 13:14:30 -0700

    task 15

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-# Author - Stanley Kirega
-
-def magic_calculation(a, b, c):
-   if a < b:
-        return (c)
-    if c > b:
-        return (a + b)
-    return (a*b - c)
+def remove_char_at(str, n):
+    s = ""
+    for i in range(len(str)):
+        if i != n:
+            s = s + str[i]
+    return (s)
