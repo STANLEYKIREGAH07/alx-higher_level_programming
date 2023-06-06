899b5d43d9a5f2c88347c3fc096bff2cab304542
STANLEYKIREGAH07 <stankirega@gmail.com>
2023-06-06 12:42:00 -0700

    done

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -2,10 +2,9 @@
 #!/usr/bin/python3
 # 102-magic_calculation.py
 
-def magic_calculation(x, y, z):
-  
-    if x < y:
-        return (z)
-    if z > y:
-        return (x + y)
-    return (x*y - z)
+def magic_calculation(a, b, c):
+     if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a*b - c)
