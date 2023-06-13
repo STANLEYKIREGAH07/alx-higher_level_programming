bc55740947cec914b17cceee4a47057f96251473
STANLEYKIREGAH07 <stankirega@gmail.com>
2023-06-13 02:06:13 -0700

    done

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,6 @@
 # 8-multiple_returns.py
 
 def multiple_returns(sentence):
-   
-    if sentence == "":
-        return (0, None)
-    return (len(sentence), sentence[0])
+if sentence == "":
+return (0, None)
+return (len(sentence), sentence[0])
